0c4a008788b1ea0883cda2dc8d3fd695b0a7d145
DiegoSgammeglia <101456085+DiegoSgammeglia@users.noreply.github.com>
2024-10-07 11:05:53 +0200

    domanda 3

--- js/funzioni.js
+++ js/funzioni.js
@@ -6,11 +6,24 @@ function readJson(){
     fetch('../domande.json')
         .then((response) => response.json())
         .then((json) => {
+            let numeroDomande=2;
             let domande = json;
-            let titoloDomanda = domande.Domande[1].Domanda1;
-            let rispostaCorretta = domande.Domande[0].RispostaCorretta;
-            document.getElementById("output").innerHTML = "<h2> " + titoloDomanda + " </h2>";
-            document.getElementById("output").innerHTML = rispostaCorretta;
+            let indice = 0;
+            for(i=0; i < numeroDomande; i++){
+                let titoloDomanda = domande.Domande[i].Domanda1;
+                let rispostaCorretta = domande.Domande[i].RispostaCorretta;
+                let risposta1 = domande.Domande[i].Risposta1;
+                let risposta2 = domande.Domande[i].Risposta2;
+                let risposta3 = domande.Domande[i].Risposta3;
+                let risposta4 = domande.Domande[i].Risposta4;
+                document.getElementById("output").innerHTML +=  "<h2> " + (i+1) + ") " +  titoloDomanda + " </h2>";
+                document.getElementById("output").innerHTML += "<p> - " + risposta1 + " </p>";
+                document.getElementById("output").innerHTML += "<p> - " + risposta2 + " </p>";
+                document.getElementById("output").innerHTML += "<p> - " + risposta3 + " </p>";
+                document.getElementById("output").innerHTML += "<p> - " + risposta4 + " </p>";
+                document.getElementById("output").innerHTML += "<b>"+ rispostaCorretta + "</b>";
+            }
+            
         });
 
 
