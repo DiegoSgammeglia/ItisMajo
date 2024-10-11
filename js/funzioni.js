1fabf376b5b7054442dee11a2269488315230eb6
DiegoSgammeglia <101456085+DiegoSgammeglia@users.noreply.github.com>
2024-10-11 11:05:54 +0200

    radio button

--- js/funzioni.js
+++ js/funzioni.js
@@ -1,7 +1,6 @@
 
 
 function readJson(){
-
    
     fetch('../domande.json')
         .then((response) => response.json())
@@ -9,21 +8,30 @@ function readJson(){
             let numeroDomande=2;
             let domande = json;
             let indice = 0;
+            let divOutput =  document.getElementById("output");
+
+           
             for(i=0; i < numeroDomande; i++){
-                let titoloDomanda = domande.Domande[i].Domanda1;
+                let titoloDomanda = domande.Domande[i].Domanda;
                 let rispostaCorretta = domande.Domande[i].RispostaCorretta;
                 let risposta1 = domande.Domande[i].Risposta1;
                 let risposta2 = domande.Domande[i].Risposta2;
                 let risposta3 = domande.Domande[i].Risposta3;
-                let risposta4 = domande.Domande[i].Risposta4;
-                document.getElementById("output").innerHTML +=  "<h2> " + (i+1) + ") " +  titoloDomanda + " </h2>";
-                document.getElementById("output").innerHTML += "<p> - " + risposta1 + " </p>";
-                document.getElementById("output").innerHTML += "<p> - " + risposta2 + " </p>";
-                document.getElementById("output").innerHTML += "<p> - " + risposta3 + " </p>";
-                document.getElementById("output").innerHTML += "<p> - " + risposta4 + " </p>";
-                document.getElementById("output").innerHTML += "<b>"+ rispostaCorretta + "</b>";
+                let risposta4 = domande.Domande[i].Risposta4;               
+                divOutput.innerHTML +=  "<h2> " + (i+1) + ") " +  titoloDomanda + " </h2>";
+                divOutput.innerHTML += '<input type="radio" id="risposta1" name="risposta1" value="' + risposta1 + '">';
+                divOutput.innerHTML += '<label> '+risposta1 +'</label> <br>';
+                divOutput.innerHTML += '<input type="radio" id="risposta2" name="risposta2" value="' + risposta2 + '">';
+                divOutput.innerHTML += '<label> '+risposta2 +'</label> <br>';
+                divOutput.innerHTML += '<input type="radio" id="risposta3" name="risposta3" value="' + risposta3 + '">';
+                divOutput.innerHTML += '<label> '+risposta3 +'</label> <br>';
+                divOutput.innerHTML += '<input type="radio" id="risposta4" name="risposta4" value="' + risposta4 + '">';
+                divOutput.innerHTML += '<label> '+risposta4 +'</label> <br> ';
+               
             }
-            
+
+             divOutput.innerHTML += '  <input type="submit" value="Submit">';
+             
         });
 
 
